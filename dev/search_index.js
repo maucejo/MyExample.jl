e89601e41beae201214a472aceae6a55b3b73340
Documenter.jl <documenter@juliadocs.github.io>
2022-05-28 15:26:29 +0000

    build based on fc69bf5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MyExample","category":"page"},{"location":"#MyExample","page":"Home","title":"MyExample","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MyExample.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MyExample]","category":"page"}]
+}
