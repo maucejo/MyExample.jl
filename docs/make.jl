fc69bf5e4b5177813356a8e754f8fec47e2a9878
Mathieu Aucejo <mathieu.aucejo@gmail.com>
2022-05-28 17:22:54 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.27

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+using MyExample
+using Documenter
+
+DocMeta.setdocmeta!(MyExample, :DocTestSetup, :(using MyExample); recursive=true)
+
+makedocs(;
+    modules=[MyExample],
+    authors="Mathieu Aucejo <mathieu.aucejo@gmail.com> and contributors",
+    repo="https://github.com/maucejo/MyExample.jl/blob/{commit}{path}#{line}",
+    sitename="MyExample.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://maucejo.github.io/MyExample.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/maucejo/MyExample.jl",
+    devbranch="master",
+)
